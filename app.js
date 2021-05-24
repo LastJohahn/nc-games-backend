fb70dca094a8e614e0f8e68a35d090992a83197f
Johann Hannerth <hannerth94@gmail.com>
2021-05-24 12:03:38 +0100

    allows cross origin resource sharing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const {
   handleInvalidRouteErrors,
   handleInternalServerError,
@@ -10,6 +11,8 @@ const {
 const apiRouter = require("./server/routers/api.router.js");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
