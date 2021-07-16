876bcb87bfcae45cebe56fcec6ae71a106b04286
Johann Hannerth <hannerth94@gmail.com>
2021-07-16 14:11:52 +0100

    finish off getCommentByIdAndUpdateVotes to respond with updated comment and status 200

--- server/controllers/comments.controller.js
+++ server/controllers/comments.controller.js
@@ -3,5 +3,9 @@ const { patchCommentVotesById } = require("../models/comments.model.js");
 exports.getCommentByIdAndUpdateVotes = (req, res, next) => {
   const { comment_id } = req.params;
   const { inc_votes } = req.body;
-  patchCommentVotesById(comment_id, inc_votes);
+  patchCommentVotesById(comment_id, inc_votes)
+    .then((comment) => {
+      res.status(200).send({ comment });
+    })
+    .catch(next);
 };
