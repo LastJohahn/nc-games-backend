8508006f4f578ed83c25e833e912b606fb40b0c1
Johann Hannerth <hannerth94@gmail.com>
2021-07-16 14:09:50 +0100

    add db query statement to patchCommentVotesById

--- server/models/comments.model.js
+++ server/models/comments.model.js
@@ -1,5 +1,14 @@
 const db = require("../../db/connection.js");
 
 exports.patchCommentVotesById = async (comment_id, inc_votes) => {
-  console.log(comment_id);
+  const { rows } = await db.query(
+    `
+      UPDATE comments
+      SET votes = votes + $1
+      WHERE comment_id = $2
+      RETURNING*
+      `,
+    [inc_votes, comment_id]
+  );
+  return rows;
 };
