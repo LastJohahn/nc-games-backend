367e1cbec9fec4cb3d52bc2df364218fed1b008a
Johann Hannerth <hannerth94@gmail.com>
2021-07-17 13:09:53 +0100

    refactor selectReviewsQueryString to use format

--- db/utils/querystrings.js
+++ db/utils/querystrings.js
@@ -1,20 +1,28 @@
+const format = require("pg-format");
+
 exports.selectReviewsQueryString = (sort_by, order, category) => {
   let queryString;
   if (!category) {
-    queryString = `
+    queryString = format(
+      `
   SELECT reviews.*, COUNT(comments.comment_id)::int AS comment_count FROM reviews
   LEFT JOIN comments ON comments.review_id = reviews.review_id
   GROUP BY reviews.review_id
   ORDER BY reviews.${sort_by} ${order};
-  `;
+  `,
+      []
+    );
   } else {
-    queryString = `
+    queryString = format(
+      `
   SELECT reviews.*, COUNT(comments.comment_id)::int AS comment_count FROM reviews
   LEFT JOIN comments ON comments.review_id = reviews.review_id
-  WHERE category LIKE "${category}"
+  WHERE category LIKE %L
   GROUP BY reviews.review_id
   ORDER BY reviews.${sort_by} ${order};
-  `;
+  `,
+      [category]
+    );
   }
   return queryString;
 };
