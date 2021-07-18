708a4b6ac2e6cca44cdbf80559143ab8049c577c
Johann Hannerth <hannerth94@gmail.com>
2021-07-18 11:46:56 +0100

    finish limitSanitiser

--- db/utils/sanitisers.js
+++ db/utils/sanitisers.js
@@ -1,1 +1,6 @@
-exports.limitSanitiser = () => {};
+exports.limitSanitiser = (limit) => {
+  const parsedLimit = parseInt(limit);
+  if (!Number.isNaN(parsedLimit)) {
+    return parsedLimit.toString();
+  } else return "NaN";
+};
