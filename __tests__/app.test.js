c6066252d030ca7be932dacae5f4032216c84f75
Johann Hannerth <hannerth94@gmail.com>
2021-07-27 14:49:01 +0100

    refactor test for POST /api/reviews to account for structure of response

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -551,18 +551,16 @@ describe("POST api/reviews", () => {
       .send(reviewToSend)
       .expect(201)
       .then(({ body }) => {
-        const { review } = body;
-        expect(review[0]).toHaveProperty("review_id");
-        expect(review[0]).toHaveProperty("owner");
-        expect(review[0]).toHaveProperty("title");
-        expect(review[0]).toHaveProperty("designer");
-        expect(review[0]).toHaveProperty("category");
-        expect(review[0]).toHaveProperty("review_body");
-        expect(review[0]).toHaveProperty("created_at");
-        expect(review[0]).toHaveProperty("votes");
-        expect(review[0]).toHaveProperty("comment_count");
-        expect(review[0].author).toBe("dav3rid");
-        expect(review[0].body).toBe(
+        expect(body).toHaveProperty("review_id");
+        expect(body).toHaveProperty("owner");
+        expect(body).toHaveProperty("title");
+        expect(body).toHaveProperty("designer");
+        expect(body).toHaveProperty("category");
+        expect(body).toHaveProperty("review_body");
+        expect(body).toHaveProperty("created_at");
+        expect(body).toHaveProperty("votes");
+        expect(body.owner).toBe("dav3rid");
+        expect(body.review_body).toBe(
           "Catan needs no introduction, just trust me and start an evil sheep empire"
         );
       });
