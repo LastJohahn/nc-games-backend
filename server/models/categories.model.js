b82545f3ba51b2426e2cce033586e83c5498bcf4
Johann Hannerth <hannerth94@gmail.com>
2021-07-31 15:16:21 +0100

    complete insertCategory function

--- server/models/categories.model.js
+++ server/models/categories.model.js
@@ -1,4 +1,5 @@
 const db = require("../../db/connection.js");
+const format = require("pg-format");
 
 exports.selectCategories = () => {
   return db
@@ -12,4 +13,19 @@ exports.selectCategories = () => {
     });
 };
 
-exports.insertCategory = async (category_body) => {};
+exports.insertCategory = async (category_body) => {
+  const { slug, description } = category_body;
+  const { rows } = await db.query(
+    format(
+      `
+      INSERT INTO categories
+      (slug, description)
+      VALUES
+      (%L)
+      RETURNING*;
+      `,
+      [slug, description]
+    )
+  );
+  return rows;
+};
